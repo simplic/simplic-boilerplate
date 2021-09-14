c12edc49f0dc9733ed88c0c7e9278bd515be7385
Benedikt Eggers <be@spiegelburg.net>
2021-09-15 00:06:58 +0200

    Add project files.

---
+++ src/simplic-boilerplate/Simplic.Boilerplate.Data.DB/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simplic.Boilerplate.Data.DB
+{
+    public class Class1
+    {
+    }
+}
---
+++ src/simplic-boilerplate/Simplic.Boilerplate.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Simplic.Boilerplate.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Simplic.Boilerplate.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("134beff1-3809-42ec-8b55-32585dcef55e")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
