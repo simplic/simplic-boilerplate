18683cac08991590dc41d96026751fd139b6e547
Benedikt Eggers <be@spiegelburg.net>
2021-11-11 21:23:11 +0100

    Add basic server plugin

--- src/Simplic.PlugIn.Boilerplate.Server/Controller/ContactController.cs
+++ src/Simplic.PlugIn.Boilerplate.Server/Controller/ContactController.cs
@@ -1,10 +1,16 @@
 ﻿using Simplic.Framework.WebAPI.Core;
+using Simplic.Boilerplate;
 
 namespace Simplic.PlugIn.Boilerplate.Server
 {
     [Framework.WebAPI.JWTAuthorize]
     public class ContactController : ExtendedApiController
     {
-        
+        private readonly IContactService contactService;
+
+        public ContactController(IContactService contactService)
+        {
+            this.contactService = contactService;
+        }
     }
 }
--- src/Simplic.PlugIn.Boilerplate.Server/Hub/ContactHub.cs
+++ src/Simplic.PlugIn.Boilerplate.Server/Hub/ContactHub.cs
@@ -1,10 +1,16 @@
 ﻿using Microsoft.AspNet.SignalR;
+using Simplic.Boilerplate;
 
 namespace Simplic.PlugIn.Boilerplate.Server
 {
     [Framework.WebAPI.SignalRJWTAuthorize]
     public class ContactHub : Hub<IContactHubClient>, IContactHub
     {
-        
+        private readonly IContactService contactService;
+
+        public ContactHub(IContactService contactService)
+        {
+            this.contactService = contactService;
+        }
     }
 }
