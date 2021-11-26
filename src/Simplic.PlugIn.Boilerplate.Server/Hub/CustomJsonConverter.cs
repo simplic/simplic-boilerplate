975b74f0c6a883a7e4dadecf40afb56db3bf0759
Tobias <tobias.milde@simplic.biz>
2021-11-26 16:52:52 +0100

    Adds custom json serializer.

---
+++ src/Simplic.PlugIn.Boilerplate.Server/Hub/CustomJsonConverter.cs
@@ -0,0 +1,89 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simplic.PlugIn.Boilerplate.Server
+{
+    public class CustomJsonConverter : JsonConverter
+    {
+        public override bool CanConvert(Type objectType)
+        {
+            return true;
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            var token = JToken.FromObject(value);
+            writer.WriteStartObject();
+            foreach (var rootToken in token)
+            {
+                if (rootToken.Type == JTokenType.Property)
+                {
+                    ConvertProperties(rootToken);
+                }
+
+                rootToken.WriteTo(writer);
+            }
+        }
+
+        private static void ConvertProperties(JToken rootToken)
+        {
+            if (rootToken is JProperty)
+            {
+                var jProperty = rootToken as JProperty;
+                if (jProperty.Value.Type == JTokenType.Array)
+                {
+                    ConvertArray(jProperty.Value);
+                }
+                else if (jProperty.Value.Type == JTokenType.Object)
+                {
+                    ConvertProperties(jProperty.Value);
+                }
+                else
+                {
+                    ConvertProperty(jProperty);
+                }
+            }
+            else if(rootToken is JObject)
+            {
+                var jObject = rootToken as JObject;
+                foreach (var item in jObject.Children())
+                {
+                    ConvertProperties(item);
+                }
+            }
+        }
+
+        private static void ConvertArray(JToken token)
+        {
+            foreach (var value in token.Values())
+            {
+                if (value is JProperty)
+                {
+                    var jProperty = value as JProperty;
+                    ConvertProperties(jProperty);
+                }
+            }
+        }
+
+        private static void ConvertProperty(JProperty jProperty)
+        {
+            var ob = new JObject(
+                new JProperty("id", "0"),
+                new JProperty("user", "SuperUser"),
+                new JProperty("value", jProperty.Value)
+                );
+
+            jProperty.Value.Replace(new JArray(ob));
+        }
+    }
+}
---
+++ src/Simplic.Plugin.Boilerplate.Server.Test/Hub/CustomJsonConverterTests.cs
@@ -0,0 +1,105 @@
+﻿using FluentAssertions;
+using Newtonsoft.Json;
+using Simplic.Boilerplate.Shared;
+using Simplic.PlugIn.Boilerplate.Server;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Simplic.Plugin.Boilerplate.Server.Test
+{
+    public class CustomJsonConverterTests
+    {
+        private CustomJsonConverter jsonConverter;
+
+        public CustomJsonConverterTests()
+        {
+            jsonConverter = new CustomJsonConverter();
+        }
+
+        [Fact]
+        public void ShouldSerialize_SimpleObject()
+        {
+            var contact = new
+            {
+                Name = "Peter",
+            };
+
+            var result = JsonConvert.SerializeObject(contact, Formatting.Indented, jsonConverter);
+
+            result.Should().Be("{\r\n  \"Name\": [\r\n    {\r\n      \"id\": \"0\",\r\n      \"user\": \"SuperUser\",\r\n      \"value\": \"Peter\"\r\n    }\r\n  ]\r\n}");
+        }
+
+        [Fact]
+        public void ShouldSerialize_NestedObject()
+        {
+            var contact = new
+            {
+                Phone = new
+                {
+                    Type = "Home",
+                    Number = "123456789"
+                },
+            };
+
+            var result = JsonConvert.SerializeObject(contact, Formatting.Indented, jsonConverter);
+
+            result.Should().Be("{\r\n  \"Phone\": {\r\n    \"Type\": [\r\n      {\r\n        \"id\": \"0\",\r\n        \"user\": \"SuperUser\",\r\n        \"value\": \"Home\"\r\n      }\r\n    ],\r\n    \"Number\": [\r\n      {\r\n        \"id\": \"0\",\r\n        \"user\": \"SuperUser\",\r\n        \"value\": \"123456789\"\r\n      }\r\n    ]\r\n  }\r\n}");
+        }
+
+        [Fact]
+        public void ShouldSerialize_DoubleNestedObject()
+        {
+            var contact = new
+            {
+                Phone = new
+                {
+                    Type = new
+                    {
+                        Name = "Home",
+                        Zip = "11111"
+                    },
+                    Number = "123456789",
+                },
+            };
+
+            var result = JsonConvert.SerializeObject(contact, Formatting.Indented, jsonConverter);
+
+            result.Should().Be("{\r\n  \"Phone\": {\r\n    \"Type\": {\r\n      \"Name\": [\r\n        {\r\n          \"id\": \"0\",\r\n          \"user\": \"SuperUser\",\r\n          \"value\": \"Home\"\r\n        }\r\n      ],\r\n      \"Zip\": [\r\n        {\r\n          \"id\": \"0\",\r\n          \"user\": \"SuperUser\",\r\n          \"value\": \"11111\"\r\n        }\r\n      ]\r\n    },\r\n    \"Number\": [\r\n      {\r\n        \"id\": \"0\",\r\n        \"user\": \"SuperUser\",\r\n        \"value\": \"123456789\"\r\n      }\r\n    ]\r\n  }\r\n}");
+        }
+
+        [Fact]
+        public void ShouldSerialize_NestedObjectInLists()
+        {
+            var contact = new
+            {
+                Phone = new[]
+                {
+                    new{
+                        Type = new
+                        {
+                            Name = "Home",
+                            Zip = "11111"
+                        },
+                        Number = "123456789",
+                    },
+                    new{
+                        Type = new
+                        {
+                            Name = "Work",
+                            Zip = "22222"
+                        },
+                        Number = "777555333",
+                    }
+                },
+            };
+
+            var result = JsonConvert.SerializeObject(contact, Formatting.Indented, jsonConverter);
+
+            result.Should().Be("{\r\n  \"Phone\": [\r\n    {\r\n      \"Type\": {\r\n        \"Name\": [\r\n          {\r\n            \"id\": \"0\",\r\n            \"user\": \"SuperUser\",\r\n            \"value\": \"Home\"\r\n          }\r\n        ],\r\n        \"Zip\": [\r\n          {\r\n            \"id\": \"0\",\r\n            \"user\": \"SuperUser\",\r\n            \"value\": \"11111\"\r\n          }\r\n        ]\r\n      },\r\n      \"Number\": [\r\n        {\r\n          \"id\": \"0\",\r\n          \"user\": \"SuperUser\",\r\n          \"value\": \"123456789\"\r\n        }\r\n      ]\r\n    },\r\n    {\r\n      \"Type\": {\r\n        \"Name\": [\r\n          {\r\n            \"id\": \"0\",\r\n            \"user\": \"SuperUser\",\r\n            \"value\": \"Work\"\r\n          }\r\n        ],\r\n        \"Zip\": [\r\n          {\r\n            \"id\": \"0\",\r\n            \"user\": \"SuperUser\",\r\n            \"value\": \"22222\"\r\n          }\r\n        ]\r\n      },\r\n      \"Number\": [\r\n        {\r\n          \"id\": \"0\",\r\n          \"user\": \"SuperUser\",\r\n          \"value\": \"777555333\"\r\n        }\r\n      ]\r\n    }\r\n  ]\r\n}");
+        }
+    }
+}
